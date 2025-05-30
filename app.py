0009634a4b0f1dc4d640c7496239d0aece45d4f9
silent-mobius <alex@vaiolab.com>
2025-05-30 10:51:52 +0300

    initill app add

---
+++ app.py
@@ -0,0 +1,21 @@
+# pylint: skip-file
+
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+
+    return jsonify(success=True,message='Hello World')
+
+@app.route('/<name>')
+def hello(name):
+
+    return jsonify(success=True,message=f'Hello {name}')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=8000)
